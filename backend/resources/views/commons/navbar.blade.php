51dc2e10f4565d96c6d29f872736e8702eafb7b4
kuroshun960 <61528468+kuroshun960@users.noreply.github.com>
2021-01-23 23:53:44 +0900

    ナビゲーション作成

---
+++ backend/resources/views/commons/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="">
+    
+    <nav class="">
+        <div class="navibar">
+            <div class="navibar__inner">
+                <p class="navIncrease">増量期<span></span>日継続中</p>
+                    <div class="navMeasurement">
+                        <p><span></span>/kcal</p>
+                        <p><span></span>/P</p>
+                        <p><span></span>/F</p>
+                        <p><span></span>/C</p>
+                    </div>
+                <p class="navWeight">現在の体重<span></span>kg</p>
+            </div>
+        </div>
+    </nav>
+</header>
